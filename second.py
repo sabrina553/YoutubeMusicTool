589700e1482f7a00760be40d2b6915afa9c0f6da
sabrina553 <sabrinanianns@proton.me>
2025-02-26 08:10:05 +0000

    Code to authenticate youtube offical api from file, if not found then go to wesbite and write to file

---
+++ second.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.channels.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/code-samples#python
+
+import os
+import json
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+from google.oauth2.credentials import Credentials
+
+
+def init():
+    global youtube
+    scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = ".env/YOUR_CLIENT_SECRET_FILE.json"
+    oauth_file = ".env/oauth.json"
+
+    # Load credentials from the oauth_file
+    credentials = None
+    if os.path.exists(oauth_file):
+        with open(oauth_file, 'r') as token:
+            credentials = Credentials.from_authorized_user_info(json.load(token), scopes)
+    else:
+        # If there are no (valid) credentials available, let the user log in.
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            client_secrets_file, scopes)
+        credentials = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(oauth_file, 'w') as token:
+            token.write(credentials.to_json())
+     # Create an API client
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+def main():
+    init()    
+    request = youtube.videos().list(
+        part="snippet,contentDetails,statistics",
+        id="GfpxPWSH_qg"
+    )
+    
+    response = request.execute()
+    print("\n")
+    print(response)
+
+if __name__ == "__main__":
+    main()
