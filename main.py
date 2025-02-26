2486533c72fa903c5db8fae5a73c3fb5b7d1494d
sabrina553 <sabrinanianns@proton.me>
2025-02-26 05:53:38 +0000

    Add docstrings to YouTubeAPI, WhoSampledAPI, and MusicSampler methods for better code documentation

--- main.py
+++ main.py
@@ -6,23 +6,28 @@ from ytmusicapi import YTMusic, OAuthCredentials
 
 class YouTubeAPI:
     def __init__(self):
+        """Initialize the YouTubeAPI class."""
         self.ytm = None
         self.init()
 
     def get_api_key(self, id):
+        """Retrieve the API key from the configuration file."""
         config = configparser.ConfigParser()
         config.read(filenames='.env/config.ini')
         return config['oauth'][id]
 
     def oauth(self):
+        """Authenticate with the YouTube API using OAuth."""
         client_id = self.get_api_key("client_id")
         client_secret = self.get_api_key("client_secret")
         self.ytm = YTMusic(".env/oauth.json", oauth_credentials=OAuthCredentials(client_id=client_id, client_secret=client_secret))
 
     def init(self):
+        """Initialize the YouTube API."""
         self.oauth()
 
     def link_to_id(self, link):
+        """Convert a YouTube link to a video or playlist ID."""
         query = urlparse(link)
         if query.hostname == 'youtu.be':
             return query.path[1:]
@@ -44,14 +49,17 @@ class YouTubeAPI:
         return None
 
     def playlist_link(self, link):
+        """Extract the playlist ID from a YouTube link."""
         query = urlparse(link)
         return parse_qs(query.query)['list'][0]
 
     def readable_data(self, id):
+        """Retrieve readable data for a YouTube video."""
         song_data = self.ytm.get_song(id)
         return [song_data["videoDetails"]["title"], song_data["videoDetails"]["author"], song_data["microformat"]["microformatDataRenderer"]["urlCanonical"]]
 
     def song_search(self, my_list=[]):
+        """Search for songs on YouTube."""
         b = []
         for i in my_list:
             a = self.ytm.search(query=i, filter="songs", limit=1, ignore_spelling=True)
@@ -62,12 +70,14 @@ class YouTubeAPI:
         return b
 
     def add_to_playlist(self, pid, sid):
+        """Add songs to a YouTube playlist."""
         pid = self.playlist_link(pid)
         self.ytm.add_playlist_items(playlistId=pid, videoIds=sid, duplicates=False)
 
 
 class WhoSampledAPI:
     def __init__(self):
+        """Initialize the WhoSampledAPI class."""
         self.session = requests.Session()
         self.session.headers = {
             "Host": "www.whosampled.com",
@@ -87,11 +97,13 @@ class WhoSampledAPI:
         }
 
     def samples_url(self, data):
+        """Construct the URL for samples of a song."""
         data[0] = "-".join(data[0].split())
         data[1] = "-".join(data[1].split())
         return f"https://www.whosampled.com/{data[1]}/{data[0]}/samples"
 
     def sample_finder(self, link):
+        """Find samples for a given song link."""
         response = self.session.get(link)
         if response.status_code == 404:
             response = self.session.get(link[:-8])
@@ -102,6 +114,7 @@ class WhoSampledAPI:
         return self.parse_samples()
 
     def parse_samples(self):
+        """Parse the samples from the cached HTML."""
         with open('cache.html', 'rb') as f:
             soup = BeautifulSoup(f.read(), 'html.parser')
         samples = []
@@ -116,13 +129,16 @@ class WhoSampledAPI:
 
 class MusicSampler:
     def __init__(self):
+        """Initialize the MusicSampler class."""
         self.youtube_api = YouTubeAPI()
         self.whosampled_api = WhoSampledAPI()
 
     def convert_to_list(self, yay):
+        """Convert a single item to a list if necessary."""
         return yay if isinstance(yay, list) else [yay]
 
     def find_song_samples(self, ids):
+        """Find samples for a list of song IDs."""
         samples = []
         for id in ids:
             link_sample = self.whosampled_api.samples_url(self.youtube_api.readable_data(id))
@@ -131,6 +147,7 @@ class MusicSampler:
         return samples
 
     def read_samples(self, links, samples):
+        """Read and print samples for a list of song links."""
         for link, sample in zip(links, samples):
             if sample:
                 current_song = self.youtube_api.readable_data(link)
@@ -139,9 +156,11 @@ class MusicSampler:
                 for s in sample:
                     print(f"{s[1]} - {s[2]}")
                 print("\n")
-                #self.youtube_api.add_to_playlist("https://music.youtube.com/playlist?list=PLv9DYoydAiAG_ERsqhCXbbeEDDE8IeCbm&si=VsN7FRCoGRl92ViR", sample_video_ids)
+                # Uncomment the following line to add samples to a playlist
+                # self.youtube_api.add_to_playlist("https://music.youtube.com/playlist?list=PLv9DYoydAiAG_ERsqhCXbbeEDDE8IeCbm&si=VsN7FRCoGRl92ViR", sample_video_ids)
 
     def main(self):
+        """Main function to find and read song samples."""
         link_youtube = "https://music.youtube.com/playlist?list=OLAK5uy_nFiS1SeXBnJII-kBfpg7kGRB0JeE_tot8"  # DAMN.
         ids = self.convert_to_list(self.youtube_api.link_to_id(link_youtube))
         samples = self.find_song_samples(ids)
